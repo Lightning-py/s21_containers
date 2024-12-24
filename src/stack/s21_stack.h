c81dbe5be6a4c433ee359bd9040965d1da2998fb
Emrick Onnia <emrickon@un-h6.kzn.21-school.ru>
2024-12-24 18:02:37 +0300

    Update s21_stack

--- src/stack/s21_stack.h
+++ src/stack/s21_stack.h
@@ -1,8 +1,9 @@
 #ifndef S21_STACK_H
 #define S21_STACK_H
 
+#include "../linked_list/linked_list.h"
 namespace s21 {
-template <typename T>
+template <class T, class Container = list<T>>
 class stack {
  public:
   using value_type = T;
@@ -10,7 +11,6 @@ class stack {
   using const_reference = const T &;
   using size_type = size_t;
 
- private:
  public:
   stack();
   stack(std::initializer_list<value_type> const &items);
@@ -18,7 +18,7 @@ class stack {
   stack(stack &&s);
   ~stack();
 
-  stack &operator=(const stack &s);  // ???
+  // stack &operator=(const stack &s);
   stack &operator=(stack &&s);
 
   const_reference top() const;
@@ -27,11 +27,16 @@ class stack {
   size_type size() const noexcept;
 
   void push(const_reference value);
-  void push(value_type &&value);  // ???
+  // void push(value_type &&value);
   void pop();
   void swap(stack &s);
+
+ private:
+  Container container_;
 };
 
 }  // namespace s21
+
+#include "s21_stack.tpp"
 
 #endif  // S21_STACK_H
