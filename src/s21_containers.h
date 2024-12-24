d9b1db94e39d637999d8d7feccddf9ce25e47cdd
Emrick Onnia <emrickon@un-h6.kzn.21-school.ru>
2024-12-24 18:02:12 +0300

    Update s21_queue.h

--- src/s21_containers.h
+++ src/s21_containers.h
@@ -6,8 +6,8 @@
 #define S21_CONTAINERS_H
 
 #include "./linked_list/linked_list.h"
-#include "./s21_queue/s21_queue.h"
-#include "./s21_stack/s21_stack.h"
+#include "./queue/s21_queue.h"
+#include "./stack/s21_stack.h"
 #include "./vector/vector.h"
 
 #endif  // S21_CONTAINERS_H
